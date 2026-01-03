7fde9ae556279bc759bdb543d20bb0f92fca893b
Au-venturous-Buddy <auventurous.buddy@gmail.com>
2026-01-03 19:05:10 +0800

    Initial Commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.b739.ual.gfea.auvb',
+  appName: "Au-venturous Buddy Gotta Fly 'Em All: United Airlines B737-900(ER)",
+  webDir: 'public',
+  bundledWebRuntime: false
+};
+
+export default config;
---
+++ src/assets/index.md
@@ -0,0 +1,5 @@
+---
+title: "United Airlines B737-900(ER)"
+retired: false
+category: "Wordpress Posts"
+---
